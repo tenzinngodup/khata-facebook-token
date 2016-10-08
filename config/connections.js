54f66dc89df00afa8a86963c6839606a83dea9bb
tngodup <tenzin.ngodup.rinchentsang@gmail.com>
2016-10-08 14:27:01 -0700

    heroku postgres

--- config/connections.js
+++ config/connections.js
@@ -74,13 +74,13 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  // somePostgresqlServer: {
-  //   adapter: 'sails-postgresql',
-  //   host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_POSTGRES_USER', // optional
-  //   password: 'YOUR_POSTGRES_PASSWORD', // optional
-  //   database: 'YOUR_POSTGRES_DB' //optional
-  // }
+  PostgresqlServer: {
+    adapter: 'sails-postgresql',
+    host: process.env.pg_host,
+    user: process.env.pg_user, // optional
+    password: process.env.pg_password, // optional
+    database: process.env.pg_db //optional
+  }
 
 
   /***************************************************************************
